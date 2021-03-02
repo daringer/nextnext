d0c56f4a6304ad904b0b441ca9df678eeb77fd52
Markus Meissner <coder@safemailbox.de>
2021-03-02 22:55:31 +0100

    added prepare-harddrive.sh

---
+++ prepare_harddrive.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+uid=$(id -u)
+
+if [[ "$uid" != "0" ]]; then
+	echo "need root!"
+	exit 1
+fi
+
+if [[ "$1" = "" || "$2" != "" ]]; then
+	echo "usage: $0 <disk-device>"
+	exit 1
+fi
+
+dev=$1
+
+if [[ ! -a "$dev" ]]; then
+	echo "device: $dev does not exist, exiting..."
+	exit 1
+fi
+
+
+# create harddisk partition
+sfdisk ${dev} --delete
+echo 'label: gpt' | sfdisk ${dev}
+echo '-;-;linux' | sfdisk ${dev}
+
+# create ext4 filesystem with label
+mkfs.ext4 -L NextBoxHardDisk ${dev}1
+
+# mount & chmod 775
+mkdir -p tmp
+mount ${dev}1 tmp
+mkdir -p tmp/srv
+mkdir -p tmp/varlog
+mkdir -p tmp/varlibdocker
+mkdir -p tmp/varcache
+chmod 775 tmp
+umount tmp
+
+
+
